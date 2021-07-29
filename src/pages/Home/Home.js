4bbc45e5ddccd64ec1ed8441ed07727a92199028
Tamir Shmueli <44590894+Tamirs2702@users.noreply.github.com>
2021-07-29 20:30:50 +0300

    [*] Task 1,2,3 Complete

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,21 +3,30 @@ import Text from "components/Text";
 import UserList from "components/UserList";
 import { usePeopleFetch } from "hooks";
 import * as S from "./style";
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 const Home = () => {
-  const { users, isLoading } = usePeopleFetch();
+  const { users, isLoading, fetchUsers } = usePeopleFetch();
 
   return (
-    <S.Home>
-      <S.Content>
-        <S.Header>
-          <Text size="64px" bold>
-            PplFinder
-          </Text>
-        </S.Header>
-        <UserList users={users} isLoading={isLoading} />
-      </S.Content>
-    </S.Home>
+      <S.Home>
+        <S.Content>
+          <S.Header>
+            <Text size="64px" bold>
+              PplFinder
+            </Text>
+          </S.Header>
+          <InfiniteScroll
+            dataLength={users.length} //This is important field to render the next data
+            next={fetchUsers}
+            hasMore={true}
+            loader={<h4>Loading...</h4>}
+          >
+          <UserList users={users} isLoading={isLoading} />
+        </InfiniteScroll>
+        </S.Content>
+      </S.Home>
+
   );
 };
 
