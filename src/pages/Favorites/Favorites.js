a3ecfe2e68ae01b69ee88f99675e852476868d60
Maor Magori <maor@magori.online>
2022-07-21 16:59:24 +0300

    feat: ✨ Add snackbar notification when user is removed from favorites

--- src/pages/Favorites/Favorites.js
+++ src/pages/Favorites/Favorites.js
@@ -2,9 +2,19 @@ import Text from "components/Text";
 import UserList from "components/UserList";
 import * as S from "./style";
 import { useFavoritePicker } from "hooks";
+import { useEffect, useState } from "react";
+import { Snackbar } from "@material-ui/core";
 
 const Favorites = () => {
   const { addOrRemoveFavorite, isUserFavorite, favorites } = useFavoritePicker();
+  const [removedUser, setRemovedUser] = useState();
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+
+  const notifyFavoriteChange = (user) => {
+    setRemovedUser(Object.values(user.name).join(" "));
+    setOpenSnackbar(true);
+    addOrRemoveFavorite(user);
+  };
 
   return (
     <S.Home>
@@ -17,8 +27,16 @@ const Favorites = () => {
         <UserList
           users={favorites}
           isLoading={false}
-          addOrRemoveFavorite={addOrRemoveFavorite}
+          addOrRemoveFavorite={notifyFavoriteChange}
           isUserFavorite={isUserFavorite}
+        />
+        <Snackbar
+          open={openSnackbar}
+          autoHideDuration={3000}
+          onClose={() => {
+            setOpenSnackbar(false);
+          }}
+          message={`Removed ${removedUser}`}
         />
       </S.Content>
     </S.Home>
