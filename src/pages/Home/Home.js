599bc94334e2f1bddd5dc99eaebed18516f8e2a1
Maor Magori <maor@magori.online>
2022-07-21 16:28:27 +0300

    refactor: 🏗️ Remove favorites hook from userList and move to parent

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import Text from "components/Text";
 import UserList from "components/UserList";
-import { usePeopleFetch } from "hooks";
+import { useFavoritePicker, usePeopleFetch } from "hooks";
 import * as S from "./style";
+import { NATIONALITIES as countries } from "constant";
 
 const Home = () => {
   // Added user checked countries to component's state and passed it to the usePeopleHook.
   const [userCountries, setUserCountries] = useState([]);
   const { users, isLoading } = usePeopleFetch(userCountries);
+  const { addOrRemoveFavorite, isUserFavorite } = useFavoritePicker();
 
   return (
     <S.Home>
@@ -22,6 +24,9 @@ const Home = () => {
           isLoading={isLoading}
           setUserCountries={setUserCountries}
           userCountries={userCountries}
+          countries={countries}
+          addOrRemoveFavorite={addOrRemoveFavorite}
+          isUserFavorite={isUserFavorite}
         />
       </S.Content>
     </S.Home>
