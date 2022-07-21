64ec80157564377ca9f39563819d94f0bfc7227c
Maor Magori <maor@magori.online>
2022-07-21 16:32:08 +0300

    feat: ✨ Rewrote favorites component to display current favorites

--- src/pages/Favorites/Favorites.js
+++ src/pages/Favorites/Favorites.js
@@ -1,31 +1,28 @@
-import React, { useState } from "react";
 import Text from "components/Text";
 import UserList from "components/UserList";
-import { usePeopleFetch } from "hooks";
 import * as S from "./style";
+import { useFavoritePicker } from "hooks";
 
-const Home = () => {
-  // Added user checked countries to component's state and passed it to the usePeopleHook.
-  const [userCountries, setUserCountries] = useState([]);
-  const { users, isLoading } = usePeopleFetch(userCountries);
+const Favorites = () => {
+  const { addOrRemoveFavorite, isUserFavorite, favorites } = useFavoritePicker();
 
   return (
     <S.Home>
       <S.Content>
         <S.Header>
           <Text size="64px" bold>
-            PplFinder
+            My Fav Ppl
           </Text>
         </S.Header>
         <UserList
-          users={users}
-          isLoading={isLoading}
-          setUserCountries={setUserCountries}
-          userCountries={userCountries}
+          users={favorites}
+          isLoading={false}
+          addOrRemoveFavorite={addOrRemoveFavorite}
+          isUserFavorite={isUserFavorite}
         />
       </S.Content>
     </S.Home>
   );
 };
 
-export default Home;
+export default Favorites;
